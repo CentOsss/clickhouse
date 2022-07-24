bd6b9d7344b92b65068caf6f1021f9e79c778949
CentOsss <44666536+CentOsss@users.noreply.github.com>
2022-07-24 22:48:59 +0300

    Initial commit

---
+++ terraform-yc/variables.tf
@@ -0,0 +1,11 @@
+variable "yandex_cloud_id" {
+  default = "b1gkbfd9nj8qji9ij74j"
+}
+
+variable "yandex_folder_id" {
+  default = "b1g1uide9uel0o2fmtl3"
+}
+
+variable "centos-7" {
+  default = "fd8ksb92cu689husemj7"
+}
